9f9d7ed9fbbf5c7aa8e0ca0ec659cfdf470417be
Marlon Alagoda <marlon.alagoda@senacor.com>
2018-02-20 19:15:13 +0100

    Explicitly define headers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const path = require('path')
 // Don't do that!
 const fetchTemplateFs = require('../node_modules/node-tailor/lib/fetch-template')
+const ACCEPT_HEADERS = [
+  'accept-language',
+  'referer',
+  'user-agent',
+  'x-request-uri',
+  'x-request-host',
+  'cookie'
+]
 
 module.exports = {
   fetchTemplate: fetchTemplateFs(
@@ -11,8 +19,14 @@ module.exports = {
   filterRequestHeaders: (attributes, request) => {
     const { public: publicFragment } = attributes
     const { headers = {} } = request
+    console.log(headers)
 
     // Headers are not forwarded to public fragment for security reasons
-    return publicFragment ? {} : headers
+    return publicFragment
+      ? {}
+      : ACCEPT_HEADERS.reduce((newHeaders, key) => {
+        headers[key] && (newHeaders[key] = headers[key])
+        return newHeaders
+      }, {})
   }
 }
